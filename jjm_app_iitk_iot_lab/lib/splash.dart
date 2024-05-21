7ebde90f698424c859f48860dca3856fec2f11d9
Devansh <devanshv17@gmail.com>
2024-05-21 19:40:28 +0530

    splash screen pushed

--- jjm_app_iitk_iot_lab/lib/splash.dart
+++ jjm_app_iitk_iot_lab/lib/splash.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:lottie/lottie.dart';
-import 'package:jjm_app_iitk_iot_lab/main.dart';
+import 'main.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}); // Adjusted constructor
 
   @override
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
+      duration: 1500,
       splash: Center(
-        child:
-         LottieBuilder.asset(
-          "assets/Flow.json",
-          width: MediaQuery.of(context).devicePixelRatio * 150.0,
-          height: MediaQuery.of(context).devicePixelRatio * 750.0,
-          fit: BoxFit.cover,
-          animate: true,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.asset(
+                "assets/Flow.json",
+                width: MediaQuery.of(context).size.width * 0.75, // Adjust size as needed
+                height: MediaQuery.of(context).size.height * 0.75, // Adjust size as needed
+                fit: BoxFit.cover,
+                animate: true, // Double the speed
+                repeat: false, // Play animation only once
+              ),
+            ],
+          ),
         ),
       ),
-      nextScreen: const MyApp(),
+      nextScreen: SecondScreen(), // Navigate to SecondScreen after splash
       splashIconSize: 400,
       backgroundColor: Color.fromARGB(255, 107, 81, 223),
     );
   }
 }
-
